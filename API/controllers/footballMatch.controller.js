3d2ee5216ddd0e27ba5a74689ad33b27cf48bd24
Oscar <oscarlp393@gmail.com>
2023-10-18 18:05:47 +0000

    changes in controllers

--- API/controllers/footballMatch.controller.js
+++ API/controllers/footballMatch.controller.js
@@ -10,7 +10,13 @@ const getAllFootballMatchs = async (req, res) => {
         const footballMatchs = await FootballMatch.findAll({
             where: req.query,
             attributes: {
-                exclude: ['played', 'goals_away', 'goals_local', 'red_card_local', 'red_card_away', 'yellow_card', 'penalties', 'refereeTeamId']
+                exclude: ['played', 'goals_away', 'goals_local', 'red_card_local', 'red_card_away', 'yellow_card', 'penalties', 'refereeTeamId',"referee_score"]
+            },
+            include : {
+                model : Team,
+                through: {
+                    attributes: []
+                  }              
             }
         })
         if (footballMatchs) {
@@ -26,7 +32,13 @@ const getAllFootballMatchs = async (req, res) => {
 const getAllFootballMatchsAllInfo = async (req, res) => {
     try {
         const footballMatchs = await FootballMatch.findAll({
-            where: req.query
+            where: req.query,
+            include : {
+                model : Team, 
+                through: {
+                    attributes: []
+                  }    
+            }
         })
         if (footballMatchs) {
             return res.status(200).json(footballMatchs)
@@ -42,7 +54,13 @@ const getOneFootballMatch = async (req, res) => {
     try {
         const footballMatch = await FootballMatch.findByPk(req.params.footballMatchId, {
             attributes: {
-                exclude: ['played', 'goals_away', 'goals_local', 'red_card_local', 'red_card_away', 'yellow_card', 'penalties', 'refereeTeamId']
+                exclude: ['played', 'goals_away', 'goals_local', 'red_card_local', 'red_card_away', 'yellow_card', 'penalties', 'refereeTeamId',"referee_score"]
+            },
+            include : {
+                model : Team,
+                through: {
+                    attributes: []
+                  }    
             }
         })
         if (footballMatch) {
@@ -57,7 +75,14 @@ const getOneFootballMatch = async (req, res) => {
 
 const getOneFootballMatchAllInfo = async (req, res) => {
     try {
-        const footballMatch = await FootballMatch.findByPk(req.params.footballMatchId)
+        const footballMatch = await FootballMatch.findByPk(req.params.footballMatchId,{
+            include : {
+                model : Team,
+                through: {
+                    attributes: []
+                  }    
+            }
+        })
         if (footballMatch) {
             return res.status(200).json(footballMatch)
         } else {
@@ -73,6 +98,12 @@ const getOwnFootballMatches= async (req, res) => {
         const footballMatches = await FootballMatch.findAll({
             where: {
                 refereeTeamId : res.locals.user.refereeTeamId
+            },
+            include : {
+                model : Team,
+                through: {
+                    attributes: []
+                  }    
             }
         })
 
@@ -91,17 +122,26 @@ const getNextFootballMatch = async (req,res) => {
     try {
         const footballMatches = await FootballMatch.findAll({
             where: {
-                refereeTeamId : res.locals.user.refereeTeamId
+                refereeTeamId : res.locals.user.refereeTeamId,
+                played : false
+            },
+            attributes:{
+                exclude : ['played', 'goals_away', 'goals_local', 'red_card_local', 'red_card_away', 'yellow_card', 'penalties',"referee_score"]
+            },
+            include : {
+                model : Team,
+                through: {
+                    attributes: []
+                  }    
             }
         })
-        let result = footballMatches.sort(function(a,b){
-            return new Date(a.date) - new Date(b.date)  ;
+        let resultArray = footballMatches.sort(function(a,b){
+            return  new Date(b.date) - new Date(a.date);
           });
-
         if (footballMatches) {
-            return res.status(200).json(result[0])
+            return res.status(200).json(resultArray[0])
         } else {
-            return res.status(404).send("FootballMatch not found")
+            return res.status(404).send("You don´t have any assigned Football Match")
         }
     } catch (error) {
         res.status(500).json({ message: error.message })
@@ -115,7 +155,7 @@ const createFootballMatch = async (req, res) => {
     try {
         const footballMatch = await FootballMatch.create(req.body)
         const refereeTeam = await RefereeTeam.findByPk(footballMatch.refereeTeamId)
-        if(refereeTeam){
+        if(refereeTeam && footballMatch.played){
             var users = await refereeTeam.getUsers()
             for(let i=0;i < users.length;i++){
                 let user = await User.findByPk(users[i].id)
@@ -126,21 +166,20 @@ const createFootballMatch = async (req, res) => {
                 stat.red_card += footballMatch.red_card_local + footballMatch.red_card_away
                 stat.penalties += footballMatch.penalties
                 stat.referee_score = (stat.referee_score + footballMatch.referee_score )/2
-                await stat.save()
-            }
+                await stat.save()   }
         }
         const team_local = await Team.findByPk(req.body.team_local)
         const team_away = await Team.findByPk(req.body.team_away)
-
         await footballMatch.addTeams(team_local)
         await footballMatch.addTeams(team_away)
 
+        if(footballMatch.played){
         team_local.sending_off = footballMatch.red_card_local 
         team_away.sending_off = footballMatch.red_card_away
 
         await team_local.save()
         await team_away.save()
-        
+    }
         if (footballMatch) {
             return res.status(200).json(footballMatch)
         } else {
--- API/controllers/refereeStats.controller.js
+++ API/controllers/refereeStats.controller.js
@@ -1,88 +1,93 @@
 const RefereeStats = require('../models/refereeStats.models')
 
 
-const getAllRefereeStats = async(req,res)=>{
+const getAllRefereeStats = async (req, res) => {
     try {
-        const refereeStats = await RefereeStats.findAll({where: req.query})
-        if(refereeStats){
+        const refereeStats = await RefereeStats.findAll({ where: req.query })
+        if (refereeStats) {
             return res.status(200).json(RefereeStats)
-        }else{
+        } else {
             return res.status(404).send("RefereeStat not found")
         }
     } catch (error) {
-        res.status(500).json({message : error.message})
+        res.status(500).json({ message: error.message })
     }
 }
 
-const getOneRefereeStat = async(req,res)=>{
+const getOneRefereeStat = async (req, res) => {
     try {
         const refereeStat = await RefereeStats.findByPk(req.params.refereeStatsId)
-        if(refereeStat){
+        if (refereeStat) {
             return res.status(200).json(refereeStat)
-        }else{
+        } else {
             return res.status(404).send("RefereeStat not found")
         }
     } catch (error) {
-        res.status(500).json({message : error.message})
-    }
-}
-const getOwnRefereeStats = async(req,res)=>{
-    try {
-        const refereeStat = await RefereeStats.findByPk(res.locals.user.id)
-        if(refereeStat){
-            return res.status(200).json(refereeStat)
-        }else{
-            return res.status(404).send("RefereeStat not found")
-        }
-    } catch (error) {
-        res.status(500).json({message : error.message})
+        res.status(500).json({ message: error.message })
     }
 }
 
-const createRefereeStat = async(req,res)=>{
+const getOwnRefereeStats = async (req, res) => {
+    try {
+        const refereeStat = await RefereeStats.findAll({
+            where: {
+                userId: res.locals.user.id
+            }
+        })
+        if (refereeStat) {
+            return res.status(200).json(refereeStat)
+        } else {
+            return res.status(404).send("RefereeStat not found")
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+const createRefereeStat = async (req, res) => {
     try {
         const refereeStat = await RefereeStats.create(req.body)
-        if(refereeStat){
+        if (refereeStat) {
             return res.status(200).json(refereeStat)
-        }else{
+        } else {
             return res.status(404).send("RefereeStat not created")
         }
     } catch (error) {
-        res.status(500).json({message : error.message})
+        res.status(500).json({ message: error.message })
     }
 }
 
-const updateRefereeStat = async(req,res)=>{
+const updateRefereeStat = async (req, res) => {
     try {
-        const refereeStat = await RefereeStats.update(req.body,{
-            where : {
-                id : req.params.refereeStatsId
+        const refereeStat = await RefereeStats.update(req.body, {
+            where: {
+                id: req.params.refereeStatsId
             }
         })
-        if(refereeStat){
-            return res.status(200).json({message : "RefereeStat updated"})
-        }else{
+        if (refereeStat) {
+            return res.status(200).json({ message: "RefereeStat updated" })
+        } else {
             return res.status(404).send("RefereeStat not found")
         }
     } catch (error) {
-        res.status(500).json({message : error.message})
+        res.status(500).json({ message: error.message })
     }
 }
 
-const deleteRefereeStat = async(req,res)=>{
+const deleteRefereeStat = async (req, res) => {
     try {
         const refereeStat = await RefereeStats.destroy({
-            where : {
-                id : req.params.refereeStatsId
+            where: {
+                id: req.params.refereeStatsId
             }
         })
-        if(refereeStat){
-            return res.status(200).json({message : "RefereeStat deleted"})
-        }else{
+        if (refereeStat) {
+            return res.status(200).json({ message: "RefereeStat deleted" })
+        } else {
             return res.status(404).send("RefereeStat not found")
         }
     } catch (error) {
-        res.status(500).json({message : error.message})
+        res.status(500).json({ message: error.message })
     }
 }
 
